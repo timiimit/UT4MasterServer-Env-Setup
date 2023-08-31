7126d250b7d5b318360712887385c4bb478916ab
timiimit <timi.korda@gmail.com>
2023-08-31 18:12:04 +0200

    Fix docker install script

--- setup/install_docker.sh
+++ setup/install_docker.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 echo "Installing docker..."
-yum install -y docker
+yum -q -y install docker
 
 # download and install docker-compose manually from their github
-curl -SL https://github.com/docker/compose/releases/download/v2.15.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# make docker-compose command available
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # make sure docker daemon will be running on next reboot
 systemctl enable docker
