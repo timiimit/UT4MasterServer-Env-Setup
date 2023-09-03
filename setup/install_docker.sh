02644b67d9329195b3db476157165826ffb062d2
timiimit <timi.korda@gmail.com>
2023-09-03 11:15:50 +0200

    Install update-docker-compose command

--- setup/install_docker.sh
+++ setup/install_docker.sh
@@ -3,9 +3,16 @@
 echo "Installing docker..."
 dnf -q -y install docker
 
-# download and install docker-compose manually from their github
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# install a script for installing/updating docker-compose
+cat >/usr/local/bin/update-docker-compose << "EOF"
+#!/bin/bash
+curl -SL -o /usr/local/bin/docker-compose https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m)
+chmod 755 /usr/local/bin/docker-compose
+EOF
+chmod 755 /usr/local/bin/update-docker-compose
+
+# run installed script
+update-docker-compose
 
 # make sure docker daemon will be running on next reboot
 systemctl enable docker
